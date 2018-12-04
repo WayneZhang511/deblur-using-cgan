19a4c559690c448b45748ec21cc208315c842e26
Yufeng Zhang <waynezhang511@gmail.com>
2018-12-04 18:14:57 -0500

    update evaluation

--- evaluation.py
+++ evaluation.py
@@ -48,5 +48,42 @@ def calculate_avg_psnr(dir):
   print('Ground truth: psnr %f \n' % (sum_gt_psnr / size))
   print('Output: psnr %f \n' % (sum_op_psnr / size))
 
-calculate_avg_psnr('test')
+# for seperated images in different folders
+def calculate_avg_psnr_sep(input_img_dir, ground_truth_dir, output_img_dir):
+  image_paths = glob(os.path.join(output_img_dir, 'gopro_*.png'))
+
+  sum_gt_psnr = 0
+  sum_gt_ssim = 0
+  sum_op_psnr = 0
+  sum_op_ssim = 0
+
+  for image_path in image_paths:
+    output_img = scipy.misc.imread(image_path)
+    image_name = os.path.basename(image_path)
+
+    input_img = scipy.misc.imread(os.path.join(input_img_dir, image_name))
+    ground_truth = scipy.misc.imread(os.path.join(ground_truth_dir, image_name))
+
+    gt_psnr = psnr(input_img, ground_truth)
+    #gt_ssim = ssim(input_img, ground_truth)
+    print('Ground truth: psnr %f\n' % (gt_psnr))
+
+    op_psnr = psnr(ground_truth, output_img)
+    #op_ssim = ssim(input_img, output_img)
+    print('Output: psnr %f \n' % (op_psnr))
+
+    sum_gt_psnr += gt_psnr
+    #sum_gt_ssim += gt_ssim
+    sum_op_psnr += op_psnr
+    #sum_op_ssim += op_ssim
+
+  size = len(image_paths)
+  print('\n')
+  print('#### Average ####')
+  print('Ground truth: psnr %f \n' % (sum_gt_psnr / size))
+  print('Output: psnr %f \n' % (sum_op_psnr / size))
+
+# calculate_avg_psnr('test')
+
+calculate_avg_psnr_sep('blur_gamma_test', 'sharp_test', 'deblurred-sample_blur_test')
 
