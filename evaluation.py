ce46f745f5c3645f9ba4a0771b9dfa021326cb3a
Yufeng Zhang <waynezhang511@gmail.com>
2018-12-08 23:43:31 -0500

    update eval

--- evaluation.py
+++ evaluation.py
@@ -6,6 +6,7 @@ from scipy import ndimage
 import numpy
 import math
 import tensorflow as tf
+import time
 
 # for concatencated 3 images
 def calculate_avg_psnr(dir):
@@ -72,21 +73,43 @@ def calculate_avg_psnr_sep(input_img_dir, ground_truth_dir, output_img_dir):
   file_dir = os.path.split(output_img_dir)[0]
 
   f = open(os.path.join(file_dir, "eval.txt"), "w")
-  with tf.Session():
+  with tf.Session() as sess:
+
+    print("Build model...")
+    start_time = time.time()
+    output_img = tf.placeholder(tf.uint8,
+                                [720, 1280, 3],
+                                name='output_img')
+    input_img = tf.placeholder(tf.uint8,
+                                [720, 1280, 3],
+                                name='input_img')
+    ground_truth = tf.placeholder(tf.uint8,
+                                [720, 1280, 3],
+                                name='ground_truth')
+    ssim = tf.image.ssim(input_img, ground_truth, max_val=255)
+    ms_ssim = tf.image.ssim_multiscale(input_img, ground_truth, max_val=255)
+    psnr = tf.image.psnr(input_img, ground_truth, max_val=255)
+    ssim1 = tf.image.ssim(output_img, ground_truth, max_val=255)
+    ms_ssim1 = tf.image.ssim_multiscale(output_img, ground_truth, max_val=255)
+    psnr1 = tf.image.psnr(output_img, ground_truth, max_val=255)
+    print(time.time() - start_time)
+
     for image_path in image_paths:
       image_name = os.path.basename(image_path)
+      op = scipy.misc.imread(image_path)
+      ip = scipy.misc.imread(os.path.join(input_img_dir, image_name))
+      gt = scipy.misc.imread(os.path.join(ground_truth_dir, image_name))
 
-      output_img = tf.image.decode_png(tf.read_file(image_path))
-      input_img = tf.image.decode_png(tf.read_file(os.path.join(input_img_dir, image_name)))
-      ground_truth = tf.image.decode_png(tf.read_file(os.path.join(ground_truth_dir, image_name)))
-
-      ssim = tf.image.ssim(input_img, ground_truth, max_val=255)
-      ms_ssim = tf.image.ssim_multiscale(input_img, ground_truth, max_val=255)
-      psnr = tf.image.psnr(input_img, ground_truth, max_val=255)
-      ssim1 = tf.image.ssim(output_img, ground_truth, max_val=255)
-      ms_ssim1 = tf.image.ssim_multiscale(output_img, ground_truth, max_val=255)
-      psnr1 = tf.image.psnr(output_img, ground_truth, max_val=255)
-      ip_ssim, ip_ms_ssim, ip_psnr, op_ssim, op_ms_ssim, op_psnr = sess.run([ssim, ms_ssim, psnr, ssim1, ms_ssim1, psnr1])
+      # ssim = tf.image.ssim(input_img, ground_truth, max_val=255)
+      # ms_ssim = tf.image.ssim_multiscale(input_img, ground_truth, max_val=255)
+      # psnr = tf.image.psnr(input_img, ground_truth, max_val=255)
+      # ssim1 = tf.image.ssim(output_img, ground_truth, max_val=255)
+      # ms_ssim1 = tf.image.ssim_multiscale(output_img, ground_truth, max_val=255)
+      # psnr1 = tf.image.psnr(output_img, ground_truth, max_val=255)
+      print("Calculate...")
+      start_time = time.time()
+      ip_ssim, ip_ms_ssim, ip_psnr, op_ssim, op_ms_ssim, op_psnr = sess.run([ssim, ms_ssim, psnr, ssim1, ms_ssim1, psnr1], {input_img:ip, output_img:op, ground_truth:gt})
+      print(time.time() - start_time)
       #ip_ssim = ssim.eval()
       #ip_ms_ssim = ms_ssim.eval()
       #ip_psnr = psnr.eval()
