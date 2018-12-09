b5318a72c1f2e9b8e4ab3ee2214f79e05c37725a
waynezhang511 <waynezhang511@gmail.com>
2018-12-09 04:10:09 +0000

    update evaluaiton

--- evaluation.py
+++ evaluation.py
@@ -68,7 +68,10 @@ def calculate_avg_psnr_sep(input_img_dir, ground_truth_dir, output_img_dir):
   sum_op_psnr = 0
   sum_op_ssim = 0
   sum_op_msssim = 0
+  
+  file_dir = os.path.split(output_img_dir)[0]
 
+  f = open(os.path.join(file_dir, "eval.txt"), "w")
   with tf.Session():
     for image_path in image_paths:
       image_name = os.path.basename(image_path)
@@ -77,26 +80,34 @@ def calculate_avg_psnr_sep(input_img_dir, ground_truth_dir, output_img_dir):
       input_img = tf.image.decode_png(tf.read_file(os.path.join(input_img_dir, image_name)))
       ground_truth = tf.image.decode_png(tf.read_file(os.path.join(ground_truth_dir, image_name)))
 
-      ssim = tf.image.ssim(input_img, groud_truth, max_val=255)
-      ms_ssim = tf.image.ssim_multiscale(input_img, groud_truth, max_val=255)
-      psnr = tf.image.psnr(input_img, groud_truth, max_val=255)
-
-      ip_ssim = ssim.eval()
-      ip_ms_ssim = ms_ssim.eval()
-      ip_psnr = psnr.eval()
-
+      ssim = tf.image.ssim(input_img, ground_truth, max_val=255)
+      ms_ssim = tf.image.ssim_multiscale(input_img, ground_truth, max_val=255)
+      psnr = tf.image.psnr(input_img, ground_truth, max_val=255)
+      ssim1 = tf.image.ssim(output_img, ground_truth, max_val=255)
+      ms_ssim1 = tf.image.ssim_multiscale(output_img, ground_truth, max_val=255)
+      psnr1 = tf.image.psnr(output_img, ground_truth, max_val=255)
+      ip_ssim, ip_ms_ssim, ip_psnr, op_ssim, op_ms_ssim, op_psnr = sess.run([ssim, ms_ssim, psnr, ssim1, ms_ssim1, psnr1])
+      #ip_ssim = ssim.eval()
+      #ip_ms_ssim = ms_ssim.eval()
+      #ip_psnr = psnr.eval()
+      print(image_name)
       print('Input: psnr %f, ssim %f, ms_ssim %f' % (ip_psnr, ip_ssim, ip_ms_ssim))
       
-      ssim = tf.image.ssim(output_img, groud_truth, max_val=255)
-      ms_ssim = tf.image.ssim_multiscale(output_img, groud_truth, max_val=255)
-      psnr = tf.image.psnr(output_img, groud_truth, max_val=255)
+      f.write(image_name + '\n')
+      f.write('Input: psnr %f, ssim %f, ms_ssim %f\n' % (ip_psnr, ip_ssim, ip_ms_ssim))
+      
+      #ssim = tf.image.ssim(output_img, ground_truth, max_val=255)
+      #ms_ssim = tf.image.ssim_multiscale(output_img, ground_truth, max_val=255)
+      #psnr = tf.image.psnr(output_img, ground_truth, max_val=255)
 
-      op_ssim = ssim.eval()
-      op_ms_ssim = ms_ssim.eval()
-      op_psnr = psnr.eval()
+      #op_ssim = ssim.eval()
+      #op_ms_ssim = ms_ssim.eval()
+      #op_psnr = psnr.eval()
 
       print('Output: psnr %f, ssim %f, ms_ssim %f\n' % (op_psnr, op_ssim, op_ms_ssim))
-
+      
+      f.write('Output: psnr %f, ssim %f, ms_ssim %f\n\n' % (op_psnr, op_ssim, op_ms_ssim))
+      
       sum_ip_psnr += ip_psnr
       sum_ip_ssim += ip_ssim
       sum_ip_msssim += ip_ms_ssim
@@ -108,8 +119,12 @@ def calculate_avg_psnr_sep(input_img_dir, ground_truth_dir, output_img_dir):
   print('#### Average ####')
   print('Input: psnr %f, ssim %f, ms_ssim %f' % (sum_ip_psnr / size, sum_ip_ssim / size, sum_ip_msssim / size))
   print('Output: psnr %f, ssim %f, ms_ssim %f \n' % (sum_op_psnr / size, sum_op_ssim / size, sum_op_msssim / size))
-
+  f.write('#### Average ####\n')
+  f.write('Input: psnr %f, ssim %f, ms_ssim %f \n' % (sum_ip_psnr / size, sum_ip_ssim / size, sum_ip_msssim / size))
+  f.write('Output: psnr %f, ssim %f, ms_ssim %f \n' % (sum_op_psnr / size, sum_op_ssim / size, sum_op_msssim / size))
+  
+  f.close()
 # calculate_avg_psnr('/Users/wayne/Desktop')
 
-calculate_avg_psnr_sep('blur_gamma_test', 'sharp_test', 'deblurred-sample_blur_test')
+calculate_avg_psnr_sep('blur_gamma_test', 'sharp_test', './deblurred-sample_blur_test')
 
